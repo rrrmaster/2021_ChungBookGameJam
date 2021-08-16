e4a236f161c890d7c5f7b47a694f38cbe26a4845
rrrmaster <dudgus9101@Naver.com>
2021-08-16 22:03:18 +0900

    1.TMP 추가 2.픽셀 퍼펙트추가 3.리소스 추가 4.플레이어 테스트 코드 작성

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rigidbody;
+
+    [SerializeField]
+    private Animator animator;
+
+
+    public float speed = 10;
+
+    private void Start()
+    {
+        Observable.EveryUpdate()
+            .Select(_ => new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0))
+            .Subscribe(v => Move(v));
+
+        Observable.EveryLateUpdate().Subscribe(_ => Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -10));
+    }
+    private void Move(Vector2 velocity)
+    {
+        animator.SetFloat("x", velocity.x);
+        animator.SetFloat("y", velocity.y);
+        animator.SetFloat("velocity", velocity.magnitude);
+
+        rigidbody.velocity = velocity.normalized * speed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Models/GameModel.cs
@@ -0,0 +1,6 @@
+using System;
+using UniRx;
+
+public class GameModel
+{
+}
